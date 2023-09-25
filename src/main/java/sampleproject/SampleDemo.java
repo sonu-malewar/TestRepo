9491f5329d3e30e8a294552af8daa30bcbe146b6
S-Malewar <malewarsonu@gmail.com>
2023-09-25 13:08:27 +0530

    swap commit

---
+++ src/main/java/sampleproject/SampleDemo.java
@@ -0,0 +1,21 @@
+package sampleproject;
+
+public class SampleDemo {
+	
+	public static void main(String[] args) {
+		
+		int a=50,b=30;
+		
+		a=a+b;  //80
+		
+		b=a-b;  // 50
+		
+		a= a-b; //30
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+
+}
